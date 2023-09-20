cf355d8b721f32aacc571e24acb54021e922ebe7
Loganrud <lrudsenske25@sandomenico.org>
2023-09-20 15:08:23 -0700

    2.1.10 In Progress

---
+++ Assets/Scripts/RandomObstacle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/************************************
+ * This Class in called RandomObstacle and its purpose is to 
+ * generate obstacles along a track for a car to drive around.
+ * Each time the game is started the obstacles will be different.
+ * This script will be attached to the obstacle GameObjects: Prop_Barrier02, Prop_Cone01,
+ * SM_Rock_Boulder_01, Barrel_02, Crate_01, Veh_Armor_Car_01, Veh_Bus_Blue_Z, Veh_Car_Blue_Z,
+ * and Veh_Ute_Red_Z
+ * 
+ * "Use the following starter code to fully implement the class to meet all the 
+ * specified objectives:" prompt, ChatGPT, September 20, 2023 version, OpenAI, September 20, 2023,
+ * https://chat.openai.com/
+ * September 20, 2023 Version 1
+ * **********************************/
+
+public class RandomObstacle : MonoBehaviour
+{
+    
+}
