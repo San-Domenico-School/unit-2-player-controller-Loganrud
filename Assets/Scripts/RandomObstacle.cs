e0c247f33e60f12c3aeb2d3881adb502b54e6a5e
Loganrud <lrudsenske25@sandomenico.org>
2023-09-22 12:00:42 -0700

    Chat GPT Script implemented

--- Assets/Scripts/RandomObstacle.cs
+++ Assets/Scripts/RandomObstacle.cs
@@ -2,21 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/************************************
- * This Class in called RandomObstacle and its purpose is to 
- * generate obstacles along a track for a car to drive around.
- * Each time the game is started the obstacles will be different.
- * This script will be attached to the obstacle GameObjects: Prop_Barrier02, Prop_Cone01,
- * SM_Rock_Boulder_01, Barrel_02, Crate_01, Veh_Armor_Car_01, Veh_Bus_Blue_Z, Veh_Car_Blue_Z,
- * and Veh_Ute_Red_Z
- * 
- * "Use the following starter code to fully implement the class to meet all the 
- * specified objectives:" prompt, ChatGPT, September 20, 2023 version, OpenAI, September 20, 2023,
- * https://chat.openai.com/
- * September 20, 2023 Version 1
- * **********************************/
-
 public class RandomObstacle : MonoBehaviour
 {
-    
+    // List of obstacle prefabs
+    [SerializeField] private GameObject[] obstaclePrefabs;
+
+    // Number of obstacles to instantiate
+    private int numberOfObstacles = 10;
+
+    // Minimum and maximum spawn position along the track
+    private float minSpawnZ = -16f;
+    private float maxSpawnZ = 383f;
+    private float minSpawnX = -8f;
+    private float maxSpawnX = 8f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InstantiateObstacles();
+    }
+
+    // This method instantiates obstacles into the game
+    private void InstantiateObstacles()
+    {
+        // Make sure there are obstacle prefabs to instantiate
+        if (obstaclePrefabs == null || obstaclePrefabs.Length == 0)
+        {
+            Debug.LogError("No obstacle prefabs assigned to the RandomObstacle script.");
+            return;
+        }
+
+        // Instantiate random obstacles
+        for (int i = 0; i < numberOfObstacles; i++)
+        {
+            // Randomly select an obstacle prefab from the list
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+
+            // Randomly generate a spawn position along the track
+            float spawnZ = Random.Range(minSpawnZ, maxSpawnZ);
+            float spawnX = Random.Range(minSpawnX, maxSpawnX);
+            Vector3 spawnPosition = new Vector3(spawnX, 2.5f, spawnZ);
+
+            // Instantiate the selected obstacle at the spawn position
+            GameObject obstacle = Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
+
+            // Attach the obstacle to the RandomObstacle GameObject as a child
+            obstacle.transform.parent = transform;
+        }
+    }
 }
